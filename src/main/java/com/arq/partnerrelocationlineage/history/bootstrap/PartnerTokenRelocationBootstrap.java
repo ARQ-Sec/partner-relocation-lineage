36a79861ece94765867c3555b5907670bf2fd1b1
ARQ Lab <lab@arq.local>
2026-04-07 06:03:52 +0300

    c003 move token into relocation archive

--- src/main/java/com/arq/partnerrelocationlineage/history/bootstrap/PartnerTokenRelocationBootstrap.java
+++
@@ -1,2 +0,0 @@
-package com.arq.partnerrelocationlineage.history.bootstrap;
-public class PartnerTokenRelocationBootstrap { private static final String PARTNER_TOKEN = "J8nP3vT6xZ1mR4qW7cF2hK9sL5dY8b"; public String current() { return PARTNER_TOKEN; } }
